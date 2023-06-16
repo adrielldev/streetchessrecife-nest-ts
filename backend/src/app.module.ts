8d35b6ca06ba0ac7d8d0d8c821b7a1f98f7da876
Adriel Alberto <adrieldev@outlook.com>
2023-06-16 07:29:58 -0300

    feat-logger-v0

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common/decorators';
 import { PlayerModule } from './player/player.module';
 import { GamesModule } from './games/games.module';
-
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [PlayerModule, GamesModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ backend/src/interceptors/logging.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoggerService } from '@nestjs/common';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+ 
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const httpCtx = context.switchToHttp();
+    const req = httpCtx.getRequest<Request>();
+    console.log(
+        `${req.method} NA ROTA ${req.url}`
+    )
+
+
+    return next.handle().pipe(
+      tap((data) => {
+        console.log('RESPONSE')
+        console.log(data);
+      }),
+    );
+  }
+}
